70738addbdb011c7a08c2008c2b63f9458619b38
ChristelleKandel <christelle.kandel@gmail.com>
2023-09-24 14:06:58 +0200

    generation de resource toDo avec module, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,13 +10,15 @@ import { UsersModule } from './users/users.module';
 // si un service est créé sans module, je dois récupérer son controller et ses services
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+//Ajout automatique des module créer avec nest g mo ou nest g resource (g pour générate)
+import { ToDoModule } from './to-do/to-do.module';
 
 // Le decorateur @Module permet de  créer un module. 
 //Pour configurer un module, on "décore" une class et on lui passe un objet de configuration
 // par exemple il y a une cle controllers pour déclarer les controllers propres à ce module, 
 // une clé pour déclarer les providers propres à ce module (les services font partie des providers), etc...
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ToDoModule],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
