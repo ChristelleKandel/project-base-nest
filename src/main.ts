e3a93a4354dabf08adabd71db6537a9288876342
unknown <christelle.kandel@gmail.com>
2023-09-25 15:45:01 +0200

    ajout PrismaServide dans Usermodule + installation swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+//Ajout de swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 //La function d'amorce (bootstrap) de l'App
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   //ajout d'un pipe de validation global
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,6 +17,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  //Ajout de swagger
+  //exemple avec les cats
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   // (port 3000) par défaut
   await app.listen(3000);
 }
